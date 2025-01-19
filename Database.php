f0a749c54711392805b3b32cb8690864eb00fb68
Aayush <aausgh@gmail.com>
2025-01-19 21:00:39 +0545

    database config

--- Database.php
+++ Database.php
@@ -1,16 +1,17 @@
 <?php
 
 // connect to MYSQL database and execute a query
-
 class Database
 {
    public $connection;
-   public function __construct()
-   {
-      $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;password=2057;charset=utf8mb4";
 
-      // Create a new PDO instance with the DSN
-      $this->connection = new PDO($dsn);
+   public function __construct($config, $username = 'root', $password = '2057')
+   {
+      $dsn = 'mysql:' . http_build_query($config, '', ';');
+
+      $this->connection = new PDO($dsn, $username, $password, [
+         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      ]);
    }
 
    public function query($query)
--- index.php
+++ index.php
@@ -4,12 +4,9 @@ require 'functions.php';
 // require 'router.php';
 require 'Database.php';
 
+$config = require 'config.php';
 
+$db = new Database($config['database']);
+$posts = $db->query("select * from posts")->fetchAll();;
 
-$db = new Database();
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);;
-
-
-foreach ($posts as $post) {
-   echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts);
