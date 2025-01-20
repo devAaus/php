2cfc25955f11402ca3bc32f84ca4f2dc38382b7c
Aayush <aausgh@gmail.com>
2025-01-20 21:44:19 +0545

    form validation

--- controllers/post-create.php
+++ controllers/post-create.php
@@ -1,16 +1,26 @@
 <?php
 
+require 'Validator.php';
+
 $config = require 'config.php';
 $db = new Database($config['database']);
 
 $heading = 'Create Post';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-   $db->query('INSERT INTO posts(title, user_id) VALUES(:title, :user_id)', [
-      'title' => $_POST['title'],
-      'user_id' => 1,
+   $errors = [];
 
-   ]);
+   if (!Validator::string($_POST['title'], 1, 100)) {
+      $errors['title'] = 'Title should be greater than 1 and less than 100 characters';
+   }
+
+   if (empty($errors)) {
+      $db->query('INSERT INTO posts(title, user_id) VALUES(:title, :user_id)', [
+         'title' => $_POST['title'],
+         'user_id' => 1,
+
+      ]);
+   }
 }
 
 require "views/post-create.view.php";
--- views/post-create.view.php
+++ views/post-create.view.php
@@ -4,17 +4,33 @@
 
 <main>
    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <form method="POST">
-         <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div class="sm:col-span-3">
-               <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+      <div class="w-full max-w-sm">
+         <form method="POST">
+            <div>
+               <label
+                  for="title"
+                  class="block text-sm/6 font-medium text-gray-900">
+                  Title
+               </label>
                <div class="mt-2">
-                  <input type="text" name="title" id="title" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+                  <input
+                     type="text"
+                     name="title"
+                     id="title"
+                     value="<?= $_POST['title'] ?? '' ?>"
+                     class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                </div>
+               <?php if (isset($errors['title'])) : ?>
+                  <p class="text-red-500 text-xs mt-2">
+                     <?= $errors['title'] ?>
+                  </p>
+               <?php endif; ?>
             </div>
-         </div>
-         <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 mt-3">Save</button>
-      </form>
+            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 mt-3">
+               Save
+            </button>
+         </form>
+      </div>
    </div>
 </main>
 
