6f4b2fd630c2e3dc1f0464cb2f9d4402a6e987b7
Aayush <aausgh@gmail.com>
2025-01-23 18:19:47 +0545

    refactor code

--- Http/Forms/LoginForm.php
+++ Http/Forms/LoginForm.php
@@ -2,23 +2,29 @@
 
 namespace Http\Forms;
 
+use Core\ValidationException;
 use Core\Validator;
 
 class LoginForm
 {
    protected $errors = [];
 
-   public function validate($email, $password)
+   public function __construct(public array $attributes)
    {
-      if (!Validator::email($email)) {
+      if (!Validator::email($attributes['email'])) {
          $this->errors['email'] = 'Invalid email address.';
       }
 
-      if (!Validator::string($password)) {
+      if (!Validator::string($attributes['password'])) {
          $this->errors['password'] = 'Password is required.';
       }
+   }
 
-      return empty($this->errors);
+   public static function validate($attributes)
+   {
+      $instance = new static($attributes);
+
+      return $instance->failed() ? $instance->throw() : $instance;
    }
 
    public function errors()
@@ -26,8 +32,20 @@ class LoginForm
       return $this->errors;
    }
 
+   public function throw()
+   {
+      ValidationException::throw($this->errors(), $this->attributes);
+   }
+
+   public function failed()
+   {
+      return count($this->errors);
+   }
+
    public function error($field, $message)
    {
       $this->errors[$field] = $message;
+
+      return $this;
    }
 }
